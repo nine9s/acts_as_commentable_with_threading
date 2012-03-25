53c931d8153c13cbfdd92b2d07e69c9cf500a439
nine9s <ninenines@suseapp.com>
2012-03-25 10:41:24 -0700

    Update lib/generators/acts_as_commentable_upgrade_migration/comment.rb

--- lib/generators/acts_as_commentable_upgrade_migration/comment.rb
+++ lib/generators/acts_as_commentable_upgrade_migration/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   acts_as_nested_set :scope => [:commentable_id, :commentable_type]
-
+  acts_as_voteable
   validates_presence_of :body
   validates_presence_of :user
 
